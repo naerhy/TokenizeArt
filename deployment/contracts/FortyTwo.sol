3c9b66608cbe7583ae895c5d9ed109736806fae9
naerhy <naerhy@protonmail.com>
2025-09-16 21:57:30 +0200

    Update contract to limit max minted nfts

--- deployment/contracts/FortyTwo.sol
+++ deployment/contracts/FortyTwo.sol
@@ -5,7 +5,8 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ERC721, ERC721URIStorage } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract FortyTwo is ERC721URIStorage, Ownable {
-  uint8 private nextTokenIndex;
+  uint8 private constant MAX_SUPPLY = 1;
+  uint8 private current_supply = 0;
   mapping(address => bool) public isAuthorized;
 
   constructor() ERC721("FortyTwo", "FT42") Ownable(_msgSender()) {}
@@ -15,10 +16,11 @@ contract FortyTwo is ERC721URIStorage, Ownable {
   }
 
   function mintNFT(string memory tokenURI) public returns (uint256) {
-    uint256 tokenIndex = nextTokenIndex++;
+    require(current_supply < MAX_SUPPLY, "Maximum number of NFT already minted");
     require(isAuthorized[_msgSender()], "Not authorized to mint a NFT");
-    _mint(_msgSender(), tokenIndex);
-    _setTokenURI(tokenIndex, tokenURI);
-    return tokenIndex;
+    uint8 index = current_supply++;
+    _mint(_msgSender(), index);
+    _setTokenURI(index, tokenURI);
+    return index;
   }
 }
